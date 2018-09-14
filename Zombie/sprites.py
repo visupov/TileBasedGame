cc5569e5aa38c34959ef5d3a8d5f3ac2515b624b
Isupov <visupov@hpe.com>
2018-09-14 17:54:48 +0200

    Add collisions with walls

--- Zombie/sprites.py
+++ Zombie/sprites.py
@@ -11,37 +11,52 @@ class Player (pg.sprite.Sprite):
         self.image.fill(BLUE)
         self.rect = self.image.get_rect()
         self.vx, self.vy = 0,0
-        self.x = x
-        self.y = y
+        self.x = x * TILESIZE
+        self.y = y * TILESIZE
 
     def get_keys(self):
         self.vx, self.vy = 0,0
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT] or keys[pg.K_a]:
             self.vx = -PLAYER_SPEED
-        elif keys[pg.K_RIGHT] or keys[pg.K_d]:
+        if keys[pg.K_RIGHT] or keys[pg.K_d]:
             self.vx = PLAYER_SPEED
-        elif keys[pg.K_UP] or keys[pg.K_w]:
+        if keys[pg.K_UP] or keys[pg.K_w]:
             self.vy = -PLAYER_SPEED
-        elif keys[pg.K_DOWN] or keys[pg.K_s]:
+        if keys[pg.K_DOWN] or keys[pg.K_s]:
             self.vy = PLAYER_SPEED
+        if self.vx != 0 and self.vy != 0:
+            self.vx *= 0.7071
+            self.vy *= 0.7071
 
-    def move(self,dx=0,dy=0):
-        if not self.collide_with_walls(dx,dy):
-            self.x += dx
-            self.y += dy
-
-    def collide_with_walls(self,dx=0,dy=0):
-        for wall in self.game.walls:
-            if wall.x == self.x + dx and wall.y == self.y + dy:
-                return True
-        return False
+    def collide_with_walls(self,direction):
+        if direction == 'x':
+            hits = pg.sprite.spritecollide(self, self.game.walls, False)
+            if hits:
+                if self.vx > 0:
+                    self.x = hits[0].rect.left - self.rect.width
+                if self.vx < 0:
+                    self.x = hits[0].rect.right
+                self.vx = 0
+                self.rect.x = self.x
+        if direction == 'y':
+            hits = pg.sprite.spritecollide(self, self.game.walls, False)
+            if hits:
+                if self.vy > 0:
+                    self.y = hits[0].rect.top - self.rect.height
+                if self.vy < 0:
+                    self.y = hits[0].rect.bottom
+                self.vy = 0
+                self.rect.y = self.y
 
     def update(self):
         self.get_keys()
         self.x += self.vx * self.game.dt
         self.y += self.vy * self.game.dt
-        self.rect.topleft = (self.x, self.y)
+        self.rect.x = self.x
+        self.collide_with_walls('x')
+        self.rect.y = self.y
+        self.collide_with_walls('y')
 
 class Wall(pg.sprite.Sprite):
     def __init__(self, game,x,y):
